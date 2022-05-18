f763cee704b7aaaf607d7ed56a306596a2e0804e
khadije <khadijeh.allahgholi@gmail.com>
2022-05-18 20:30:55 +0430

    Q8 added

---
+++ Q8-js/app.js
@@ -0,0 +1,33 @@
+// class Student{
+//    constructor(_name , _gender){
+//     this.name = _name;
+//     this.gender = _gender;
+//     }
+// }
+// //let studObj = new Student("john" , "male" ); 
+// class studObj extends Student{
+//     constructor(_name , _gender , _age){
+//         super(_name , _gender);
+//         this.age = _age;
+//     }   
+// }
+// let student1 = new Student ("john" , "male");
+// let studObj1 = new studObj ("jack" , "male" , 10);
+// console.log(student1);
+// console.log(studObj1);
+
+function Student(name , gender){
+    this.name = name;
+    this.gender = gender;
+}
+Student.prototype.age = 30; 
+let studObj = new Student();
+console.log(`studObj.age = ${studObj.age}`);
+
+let studObj2 = new Student();
+console.log(`studObj2.age = ${studObj.age}`);
+
+Student.prototype.age = 20;
+let studObj1 = new Student();
+console.log(`studObj3.age = ${studObj.age}`);
+
